0b161206c4320f09dc4d0f97922b2f8564a930bb
Apurva <apurva110497@gmail.com>
2025-09-25 16:46:02 +0530

    New Changes

--- SeleniumFramework_Github/src/test/java/com/comcast/crm/campaignstest/CreateCampaigns.java
+++ SeleniumFramework_Github/src/test/java/com/comcast/crm/campaignstest/CreateCampaigns.java
@@ -11,28 +11,32 @@ import com.comcast.crm.objectrepositoryutility.HomePage;
 
 /**
  * This class is used to create the campaigns.
+ * 
  * @author Apurva
  */
 public class CreateCampaigns extends BaseClass {
 
 	/**
 	 * This method is used to create the campaigns by entering the details.
+	 * 
 	 * @throws EncryptedDocumentException
 	 * @throws IOException
 	 */
 	@Test
-	public void createCampaigns () throws EncryptedDocumentException, IOException {
+	public void createCampaigns() throws EncryptedDocumentException, IOException {
+		System.out.println("Go to Campaigns Page");
 		HomePage hp = new HomePage(driver);
 		WebDriverUtility wlib = new WebDriverUtility();
 		wlib.mouseHover(driver, hp.getMoreLink());
 		wlib.clickOnElement(driver, hp.getCampaignsLink());
+
 		CampaignPage cp = new CampaignPage(driver);
 		cp.getCreateCampaignBtn().click();
 		String NAME = eu.getDataFromExcel("Campaigns", 1, 2);
 		cp.getCampaignName().sendKeys(NAME);
 		cp.getSaveBtn().click();
 		System.out.println("Campaigns Created");
-		
+
 	}
-	
+
 }
